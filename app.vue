23eb6b8dce9f0828046e76cfe4dcc2c7badfa75d
colinscz <19342760+colinscz@users.noreply.github.com>
2024-12-31 22:46:44 +0100

    feat: configure index and app for landing page implementation

--- app.vue
+++ app.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <NuxtLayout>
+      <Navbar />
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
+<script setup lang="ts">
+import Navbar from "~/components/Navbar.vue";
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
-
+import Hero from "~/components/Hero.vue";
+import Services from "~/components/Services.vue";
+import Benefits from "~/components/Benefits.vue";
+import Features from "~/components/Features.vue";
+import Testimonials from "~/components/Testimonials.vue";
+import HowItWorks from "~/components/HowItWorks.vue";
+import Team from "~/components/Team.vue";
+import Community from "~/components/Community.vue";
+import Pricing from "~/components/Pricing.vue";
+import Contact from "~/components/Contact.vue";
+import FAQ from "~/components/FAQ.vue";
 </script>
 
 <template>
-
+  <Hero />
+<Sponsors />
+  <Benefits />
+  <Features />
+  <Services />
+  <HowItWorks />
+  <Testimonials />
+  <Team />
+  <Community />
+  <Pricing />
+  <Contact />
+  <FAQ />
+  <Footer />
 </template>
 
 <style scoped>
