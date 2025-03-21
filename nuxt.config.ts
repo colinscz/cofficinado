09ff04cec59342844e38146c2b8b61543dd0b801
colinscz <19342760+colinscz@users.noreply.github.com>
2025-03-21 19:51:56 +0100

    feat: remove shadcn and introduce nuxt-ui-pro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,33 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/color-mode'],
-  shadcn: {
-    /**
-     * Prefix for all the imported component
-     */
-    prefix: '',
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui"
-     */
-    componentDir: './components/ui'
+  modules: [
+  '@nuxt/eslint',
+  '@nuxt/image',
+  '@nuxt/ui-pro',
+  '@nuxt/content'],
+  css: ['~/assets/css/main.css'],
+
+  mdc: {
+    highlight: {
+      noApiRoute: false
+    }
+  },
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
   }
 })
